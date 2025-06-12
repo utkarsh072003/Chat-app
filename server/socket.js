b6fb561b7b1f22e02e147d2f8fe145bb0e5b5cc0
UTKARSH KUMAR <128170769+utkarsh072003@users.noreply.github.com>
2025-06-12 23:53:48 +0530

    Update socket.js
    ..

--- server/socket.js
+++ server/socket.js
@@ -6,7 +6,7 @@ import Channel from "./models/ChannelModel.js";
 const setupSocket =(server) =>{
   const io = new SocketIOServer(server,{
     cors:{
-        origin: [https://chat-app-kappa-sandy.vercel.app],
+        origin: ["https://chat-app-kappa-sandy.vercel.app"],
         methods:["GET", "POST"],
         credentials:true,
     }
